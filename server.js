110f223323887d87d8c73d0fd380d3aaaae4a339
Olga <o.smolyakova30@gmail.com>
2021-08-21 15:40:23 -0500

    added logic for first prompt, view employees, view employees by departemnt functions

--- server.js
+++ server.js
@@ -21,16 +21,103 @@ connection.connect(function (err) {
 
   //ask question about action avaliable
   function firstPrompt(){
+    inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        "End"]
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View Employees":
+          viewEmployees();
+          break;
 
+        case "View Employees by Department":
+            employeesByDepartemnt();
+          break;
+      
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employees":
+          removeEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "End":
+          connection.end();
+          break;
+      }
+    });
   };
 
   // SELECT * FROM (all employees)
   function viewEmployees(){
+    console.log("Viewing employees\n");
+
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      firstPrompt();
+    }); 
 
   };
 
   // SELECT * FROM join by id (employees by departent)
   function employeesByDepartemnt() {
+    console.log("Viewing employees by department\n");
+
+    var query =
+      `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const departmentChoices = res.map(data => ({
+        value: data.id, name: data.name
+      }));
+  
+      console.table(res);
+      console.log("Department view succeed!\n");
+  
+      promptDepartment(departmentChoices);
+    }); 
 
   };
 
@@ -51,11 +138,11 @@ connection.connect(function (err) {
   };
 
   // add role INSERT INTO
-  function addRoole() {
+  function addRole() {
 
   };
 
-  
+
 
 
 
